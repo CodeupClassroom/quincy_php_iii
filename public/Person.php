dd317aa7939767f5bc59835092652a37ad5b2641
Justin Reich <justinreich.dev@gmail.com>
2017-08-03 13:34:49 -0500

    Add files from Classes and Objects I intro lecture

---
+++ public/Person.php
@@ -0,0 +1,19 @@
+<?php
+
+class Person
+{
+
+    // properties
+    public $firstName;
+    public $lastName;
+    public $numOfLives = 1;
+
+    // methods
+
+    public function sayHello()
+    {
+        echo "Howdy!";
+    }
+
+
+}
